cfc97326df4f0bace164638d3a885e14119cf2a4
Sujal Adate <your_github_sujaladate02@gmail.com>
2026-06-14 10:05:11 +0530

    Move backend into backend folder and fix CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cookieparser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes')
 const musicRoutes = require('./routes/music.routes')
+const cors = require ("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cookieparser());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
+
 
 app.use('/api/auth',authRoutes) 
 app.use('/api/music',musicRoutes)
