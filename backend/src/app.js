96f3ca3d250b4d05669a90635d4bccac4ef51660
Sujal Adate <your_github_sujaladate02@gmail.com>
2026-07-15 22:11:30 +0530

    Update backend for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieparser());
 app.use(cors({
-  origin: "https://spotify-frontend-bice.vercel.app/",
+  origin: "https://spotify-frontend-bice.vercel.app",
   credentials: true
 }));
 
