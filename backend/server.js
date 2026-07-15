441eebd5517ee4b61d60eeb9d980f4c60ce14591
Sujal Adate <your_github_sujaladate02@gmail.com>
2026-07-15 19:28:40 +0530

    Update backend for production

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("server is running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
